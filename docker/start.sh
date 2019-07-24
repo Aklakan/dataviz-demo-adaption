7320ba36ee993afd05d9cf47cc64e1f09af19a1a
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2019-07-24 15:30:23 +0200

    fixed

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,12 @@
 
 echo "Using SPARQL endpoint: ${ENDPOINT_URL}"
 #sed -i 's|http://localhost:7531/sparql|${ENDPOINT_URL}|g' /usr/share/nginx/html/static/js/main.146575d0.js
-sed -i 's|http://localhost:7531/sparql|${ENDPOINT_URL}|g' /var/www/html/dataviz/static/js/main.146575d0.js
+
+
+FILE=`grep -Rail localhost:7531/sparql /var/www/html/dataviz/static/js | grep js$`
+echo "Replacing endpoint in file $FILE"
+
+sed -i 's|http://localhost:7531/sparql|${ENDPOINT_URL}|g' "$FILE"
 
 nginx
 
